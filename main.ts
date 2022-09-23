cfec68edd3c45eb91b3eb16da35c4be9d7742980
soheyHashimoto <heyso1209@gmail.com>
2022-09-23 21:31:35 +0900

    [ADD]insertRow

--- main.ts
+++ main.ts
@@ -3,9 +3,15 @@ function doPost() {
   const data = JSON.parse(event.postData.contents);
   // データを記載
   const name = data.contents;
-  const createdAt = Date.now();
 
   // データを記載
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("保存シート");
+  sheet.appendRow([name, createdAt]);
+}
+
+function insertRow(name: string) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("保存シート");
+
+  const createdAt = Date.now();
   sheet.appendRow([name, createdAt]);
 }
